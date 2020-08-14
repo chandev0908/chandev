0c3e48b16d00ae55fb025e1528bc87287209cda3
chandev0908 <chandev0908@gmail.com>
2020-08-15 03:21:00 +0800

    Updates

---
+++ precache-manifest.d71227694ddf06e72b2b7b8d5fd9bf8a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc17ceb74dd95d86c384b89a05074485",
+    "url": "/chandev/index.html"
+  },
+  {
+    "revision": "413653aa29cda9956436",
+    "url": "/chandev/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "231c3672386d3eb3f25a",
+    "url": "/chandev/static/css/main.930c6f67.chunk.css"
+  },
+  {
+    "revision": "413653aa29cda9956436",
+    "url": "/chandev/static/js/2.4c5205ff.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/chandev/static/js/2.4c5205ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231c3672386d3eb3f25a",
+    "url": "/chandev/static/js/main.a2118d65.chunk.js"
+  },
+  {
+    "revision": "1ff600a50b691a9949ff",
+    "url": "/chandev/static/js/runtime-main.3a902af8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/chandev/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/chandev/static/media/reactjs.ee7cd8ed.svg"
+  },
+  {
+    "revision": "f41cf3783ba21dbb8dfef64c9b6d95cf",
+    "url": "/chandev/static/media/redux.f41cf378.png"
+  },
+  {
+    "revision": "4a79c830c12c30399d0a897b2fc03668",
+    "url": "/chandev/static/media/shape.4a79c830.svg"
+  }
+]);
